b44df3b5d2e3227b642b45ee5a2d6982c1ca1a9c
Pierre Bretéché <pbreteche@dawan.fr>
2021-05-17 17:12:35 +0200

    page liste article

---
+++ src/Controller/PostController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\PostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PostController extends AbstractController
+{
+    /**
+     * @Route("/post")
+     */
+    public function index(PostRepository $postRepository): Response
+    {
+        $posts = $postRepository->findAll();
+
+        return $this->render('post/index.html.twig', [
+            'posts' => $posts,
+        ]);
+    }
+}
