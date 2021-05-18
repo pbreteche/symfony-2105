ccfebc5bd4cb7a5437d4429b086b5d8de575ccb2
Pierre Bretéché <pbreteche@dawan.fr>
2021-05-18 09:38:46 +0200

    page article détail

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class PostController extends AbstractController
 {
     /**
-     * @Route("/post")
+     * @Route("/post", methods="GET")
      */
     public function index(PostRepository $postRepository): Response
     {
@@ -18,6 +18,26 @@ class PostController extends AbstractController
 
         return $this->render('post/index.html.twig', [
             'posts' => $posts,
+        ]);
+    }
+
+    /**
+     * @Route(
+     *     "/post/{id}",
+     *     requirements={"id":"\d+"},
+     *     methods="GET"
+     * )
+     */
+    public function show(int $id, PostRepository $postRepository): Response
+    {
+        $post = $postRepository->find($id);
+
+        if (!$post) {
+            throw $this->createNotFoundException('Article '.$id.' non trouvé');
+        }
+
+        return $this->render('post/show.html.twig', [
+            'post' => $post,
         ]);
     }
 }
