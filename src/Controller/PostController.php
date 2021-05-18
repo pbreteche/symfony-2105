bec20ff848bb9c7ae3fd1501138a9ae41d3b4376
Pierre Bretéché <pbreteche@dawan.fr>
2021-05-18 12:11:58 +0200

    liste limitée de articles, ordre décroissant

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -20,7 +20,7 @@ class PostController extends AbstractController
      */
     public function index(PostRepository $postRepository): Response
     {
-        $posts = $postRepository->findAll();
+        $posts = $postRepository->findBy([], ['createdAt' => 'DESC'], 20);
 
         return $this->render('post/index.html.twig', [
             'posts' => $posts,
