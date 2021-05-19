5879c4ac241900d35b3fcd79c04bdf3385c1f373
Pierre Bretéché <pbreteche@dawan.fr>
2021-05-19 16:19:16 +0200

    création d'un alias si plusieurs classes

---
+++ src/Service/PostSearcherDemo.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Service;
+
+class PostSearcherDemo implements PostSearcherInterface
+{
+
+    public function search(?string $keyword): array
+    {
+        return [];
+    }
+}
